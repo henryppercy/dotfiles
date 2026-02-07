0f160fbefbf3da36b7b68c9d4e68deb5f0b9291f
henryppercy <henryppercy@gmail.com>
2026-02-07 11:20:35 +0000

    feat: add auto formatting

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 function M.register(mappings)
-	local present, which_key = pcall(require, "which-key")
+    local present, which_key = pcall(require, "which-key")
 
-	if present then
-		which_key.add(mappings)
-	end
+    if present then
+        which_key.add(mappings)
+    end
 end
 
 function M.setup()
@@ -20,6 +20,9 @@ function M.setup()
     -- stay in visual mode when indenting
     keymap.set("v", "<", "<gv", { desc = "Indent left" })
     keymap.set("v", ">", ">gv", { desc = "Indent right" })
+
+    -- format
+    keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
 end
 
 return M
