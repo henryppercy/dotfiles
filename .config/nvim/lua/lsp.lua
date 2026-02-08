f188ec1a15494714dfcf0061db2c2579694c361a
henryppercy <henryppercy@gmail.com>
2026-02-08 20:59:45 +0000

    feat: disable autocomplete

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -24,17 +24,17 @@ function M.setup()
     })
 
     -- configure autocomplete, don't auto select first option
-    vim.opt.completeopt = { "menuone", "noselect", "popup" }
+    -- vim.opt.completeopt = { "menuone", "noselect", "popup" }
 
     -- enable autocomplete
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(ev)
-            local client = vim.lsp.get_client_by_id(ev.data.client_id)
-            if client:supports_method("textDocument/completion") then
-                vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-            end
-        end
-    })
+    -- vim.api.nvim_create_autocmd("LspAttach", {
+    --     callback = function(ev)
+    --         local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    --         if client:supports_method("textDocument/completion") then
+    --             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    --         end
+    --     end
+    -- })
 end
 
 return M
