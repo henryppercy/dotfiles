7855388d584a714fc299c4236daf870b2ae7ebd7
henryppercy <henryppercy@gmail.com>
2026-02-15 18:47:14 +0000

    feat: remove rounded border

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -29,7 +29,6 @@ function M.setup()
 	opt.cursorline = true -- highlight the current line
 	opt.number = true -- set numbered lines
 	opt.laststatus = 3 -- display one statusline for all windows
-	opt.winborder = "rounded" -- add rounded border to dignostics
 
     -- make TreeSitter highlight groups have higher priority than LSP semantic tokens
 	vim.highlight.priorities.treesitter = 100
