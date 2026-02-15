fc927720db9f59e5d84cb84b26933a0c1c9fb800
henryppercy <henryppercy@gmail.com>
2026-02-15 18:46:37 +0000

    feat: add highlight on yank

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 function M.setup()
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = function()
+        vim.highlight.on_yank()
+      end,
+    })
+
     vim.api.nvim_create_autocmd("BufRead", {
       pattern = "/tmp/zsh*",
       callback = function()
