5b0761957db02cae250e707b3a9d8e9df7cca634
henryppercy <henryppercy@gmail.com>
2026-02-18 20:41:12 +0000

    feat: add groups to which key

--- .config/nvim/lua/plugins/configs/which-key.lua
+++ .config/nvim/lua/plugins/configs/which-key.lua
@@ -1,28 +1,40 @@
 -- https://github.com/folke/which-key.nvim
 local M = {
-	"folke/which-key.nvim",
-	lazy = false,
+    "folke/which-key.nvim",
+    lazy = false,
+}
+
+local basic_mappings = {
+    {
+        { "<leader>b", group = "Buffer" },
+        { "<leader>c", group = "Code" },
+        { "<leader>f", group = "Find" },
+        { "<leader>g", group = "Git" },
+        { "<leader>i", group = "Install" },
+    }
 }
 
 function M.config()
-	local icons = require("icons")
+    local icons = require("icons")
 
-	require("which-key").setup({
-		preset = "helix",
-		icons = {
-			breadcrumb = icons.arrow.double_right_short, -- symbol used in the command line area that shows your active key combo
-			separator = icons.bar.vertical_center_thin, -- symbol used between a key and it's label
-			group = icons.folder.open .. " ", -- symbol prepended to a group
-		},
-		win = {
-			border = "none", -- none, single, double, shadow
-			title = true, -- does not matter unless boder != "none"
-			title_pos = "center", -- does not matter unless boder != "none"
-			padding = { 2, 6 }, -- extra window padding [top/bottom, right/left]
-		},
-		show_help = false, -- show help message on the command line when the popup is visible
-		show_keys = true, -- show the currently pressed key and its label as a message in the command line
-	})
+    require("mappings").register(basic_mappings)
+
+    require("which-key").setup({
+        preset = "helix",
+        icons = {
+            breadcrumb = icons.arrow.double_right_short, -- symbol used in the command line area that shows your active key combo
+            separator = icons.bar.vertical_center_thin,  -- symbol used between a key and it's label
+            group = icons.folder.open .. " ",            -- symbol prepended to a group
+        },
+        win = {
+            border = "none",      -- none, single, double, shadow
+            title = true,         -- does not matter unless boder != "none"
+            title_pos = "center", -- does not matter unless boder != "none"
+            padding = { 2, 6 },   -- extra window padding [top/bottom, right/left]
+        },
+        show_help = false,        -- show help message on the command line when the popup is visible
+        show_keys = true,         -- show the currently pressed key and its label as a message in the command line
+    })
 end
 
 return M
