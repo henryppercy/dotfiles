4b953eaff57a547088a36645292cf3797d1e8199
henryppercy <henryppercy@gmail.com>
2026-02-18 20:40:18 +0000

    feat: update theme

--- .config/nvim/lua/plugins/configs/teide.lua
+++ .config/nvim/lua/plugins/configs/teide.lua
@@ -1,31 +1,36 @@
 -- https://github.com/serhez/teide.nvim
 local M = {
-	"serhez/teide.nvim",
-	name = "teide.nvim",
+    "serhez/teide.nvim",
+    name = "teide.nvim",
 }
 
 function M.config()
-	local teide = require("teide")
-	teide.setup({
-		terminal_colors = true,
-		style = "darker",
-		styles = {
-			comments = { italic = true },
-			keywords = { italic = true },
-			functions = {},
-			variables = {},
+    local teide = require("teide")
+    teide.setup({
+        terminal_colors = true,
+        style = "darker",
+        styles = {
+            comments = { italic = true },
+            keywords = { italic = true },
+            functions = {},
+            variables = {},
 
-			-- Background styles: "dark", "transparent", "normal"
-			sidebars = "dark",
-			floats = "dark",
-		},
-		plugins = {
-			auto = true,
-		},
-		dim_inactive = false,
-	})
-	teide.load()
+            -- Background styles: "dark", "transparent", "normal"
+            sidebars = "dark",
+            floats = "dark",
+        },
+        plugins = {
+            auto = true,
+        },
+        dim_inactive = false,
+    })
+    teide.load()
+
+    -- can this be done in the snacks.picker config
+    local colors = require("teide.colors").setup()
+    vim.api.nvim_set_hl(0, "SnacksPicker", { bg = colors.bg_darker, nocombine = true })
+    vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = colors.fg_gutter, bg = colors.bg_darker, nocombine = true })
+    vim.api.nvim_set_hl(0, "SnacksPickerInputBorder", { fg = colors.fg_gutter, bg = colors.bg_darker, nocombine = true })
 end
 
 return M
-
