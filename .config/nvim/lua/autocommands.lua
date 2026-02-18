41d64c5df3ac1e78f43feb6bc79829edf1bdfae5
henryppercy <henryppercy@gmail.com>
2026-02-18 20:44:07 +0000

    feat: update autocommands

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -2,17 +2,28 @@ local M = {}
 
 function M.setup()
     vim.api.nvim_create_autocmd("TextYankPost", {
-      callback = function()
-        vim.highlight.on_yank()
-      end,
+        callback = function()
+            vim.highlight.on_yank()
+        end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "help",
+        callback = function()
+            vim.cmd("wincmd L")
+        end,
     })
 
     vim.api.nvim_create_autocmd("BufRead", {
-      pattern = "/tmp/zsh*",
-      callback = function()
-        vim.bo.filetype = "bash"
-      end,
+        pattern = "/tmp/zsh*",
+        callback = function()
+            vim.bo.filetype = "bash"
+        end,
     })
+
+    vim.api.nvim_create_user_command('DiffClipboard', function()
+        vim.cmd('vnew | put + | diffthis | wincmd p | diffthis')
+    end, {})
 end
 
 return M
