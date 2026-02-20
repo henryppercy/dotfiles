f31278eb17d27f2365362929328319feac7a53e3
henryppercy <henryppercy@gmail.com>
2026-02-20 15:12:25 +0000

    feat: add TODO comment plugin

---
+++ .config/nvim/lua/plugins/configs/todo-comments.lua
@@ -0,0 +1,30 @@
+local M = {
+    "folke/todo-comments.nvim",
+    event = "VeryLazy",
+}
+
+function M.config()
+    local icons = require("icons")
+
+    require("todo-comments").setup({
+        signs = false,
+        sign_priority = 90,
+        keywords = {
+            FIX = {
+                icon = icons.diagnostics.bug .. " ",
+                color = "error",
+            },
+            TODO = { icon = icons.check .. " ", color = "info" },
+            HACK = { icon = icons.fire .. " ", color = "warning" },
+            WARN = {
+                icon = icons.diagnostics.warning .. " ",
+                color = "warning",
+                alt = { "WARNING", "XXX" },
+            },
+            NOTE = { icon = icons.message .. " ", color = "hint", alt = { "INFO" } },
+            PR = { icon = icons.message .. " ", color = "hint", alt = { "INFO" } },
+        },
+    })
+end
+
+return M
