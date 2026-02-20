f8c8a78ce62cb9ecbfe4b085295c9b57dbb4d6d7
henryppercy <henryppercy@gmail.com>
2026-02-20 15:36:23 +0000

    feat: add more autoformatters

--- .config/nvim/lua/plugins/configs/conform.lua
+++ .config/nvim/lua/plugins/configs/conform.lua
@@ -1,13 +1,18 @@
 return {
-	"stevearc/conform.nvim",
-	event = "BufWritePre",
-	opts = {
-		formatters_by_ft = {
-			php = { "pint" },
-		},
-		format_on_save = {
-			timeout_ms = 3000,
-			lsp_format = "fallback",
-		},
-	},
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            go = { "goimports", "gofmt" },
+            php = { "pint" },
+        },
+        format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+        },
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+    },
 }
