58f5b1f0b42d1c528323850ed5ce673b080c232c
henryppercy <henryppercy@gmail.com>
2026-02-21 14:45:43 +0000

    feat: add markdown highlighting

---
+++ .config/nvim/lua/plugins/configs/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ---@module 'render-markdown'
+    opts = {
+        completions = { lsp = { enabled = true } },
+        sign = { enabled = false },
+    },
+}
