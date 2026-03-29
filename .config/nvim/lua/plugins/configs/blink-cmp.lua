cf551ea7b2ea84d90a2afefd084ff9968ffc2aed
henryppercy <henryppercy@gmail.com>
2026-03-29 11:58:06 +0100

    style: fmt

--- .config/nvim/lua/plugins/configs/blink-cmp.lua
+++ .config/nvim/lua/plugins/configs/blink-cmp.lua
@@ -1,34 +1,37 @@
 local M = {
-	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-	version = "1.*",
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+    version = "1.*",
 
-	opts = {
-		keymap = {
-			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["<C-e>"] = { "hide" },
-			["<CR>"] = { "accept", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-			["<C-u>"] = { "scroll_documentation_up", "fallback" },
-			["<C-d>"] = { "scroll_documentation_down", "fallback" },
-		},
-		appearance = {
-			nerd_font_variant = "mono",
-		},
-		completion = { documentation = { auto_show = true } },
-		signature = { enabled = true },
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
+    opts = {
+        keymap = {
+            ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-e>"] = { "hide" },
+            ["<CR>"] = { "accept", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+            ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+            ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+        },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+        completion = { documentation = { auto_show = true } },
+        signature = { enabled = true },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            per_filetype = {
+                markdown = {},
+            },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
 }
 
 return M
