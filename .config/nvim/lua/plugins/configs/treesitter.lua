68b42c34f834d97c07abc9c8606901af89b99698
henryppercy <henryppercy@gmail.com>
2026-03-29 12:01:17 +0100

    style: fmt

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,34 +1,34 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
-	build = ":TSUpdate",
-	lazy = false,
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
 }
 
 function M.init()
-	require("mappings").register({ "<leader>it", "<cmd>TSUpdate<cr>", desc = "Update parsers" })
-	require("mappings").register({ "<leader>iT", "<cmd>TSInstall all<cr>", desc = "Install parsers (all)" })
+    require("mappings").register({ "<leader>It", "<cmd>TSUpdate<cr>", desc = "Update parsers" })
+    require("mappings").register({ "<leader>IT", "<cmd>TSInstall all<cr>", desc = "Install parsers (all)" })
 end
 
 function M.config()
-	vim.api.nvim_create_autocmd("FileType", {
-		callback = function(args)
-			local buf = args.buf
-			local filetype = args.match
+    vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+            local buf = args.buf
+            local filetype = args.match
 
-			-- Check if a parser exists for the current language
-			local language = vim.treesitter.language.get_lang(filetype) or filetype
-			if not vim.treesitter.language.add(language) then
-				return
-			end
+            -- Check if a parser exists for the current language
+            local language = vim.treesitter.language.get_lang(filetype) or filetype
+            if not vim.treesitter.language.add(language) then
+                return
+            end
 
             -- Highlighting
-			vim.treesitter.start(buf, language)
+            vim.treesitter.start(buf, language)
 
-			-- Indentation
-			vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		end,
-	})
+            -- Indentation
+            vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+    })
 end
 
 return M
