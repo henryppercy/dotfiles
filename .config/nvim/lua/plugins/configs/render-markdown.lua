ce8a443da4974d2b24ca589fde923b2827047456
henryppercy <henryppercy@gmail.com>
2026-03-29 12:00:32 +0100

    feat: markdown styling

--- .config/nvim/lua/plugins/configs/render-markdown.lua
+++ .config/nvim/lua/plugins/configs/render-markdown.lua
@@ -2,7 +2,36 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     ---@module 'render-markdown'
     opts = {
-        completions = { lsp = { enabled = true } },
-        sign = { enabled = false },
+        sign = {
+            enabled = false
+        },
+
+        link = {
+            enabled = false,
+        },
+
+        heading = {
+            icons = { "", "", "", "", "", "" },
+            width = "block",
+
+            position = "inline",
+            left_pad = 1,
+            right_pad = 1,
+        },
+        bullet = {
+            icons = { "•", "•", "•", "•" },
+        },
+        checkbox = {
+            checked = { icon = "󰄮" },
+            unchecked = { icon = "" },
+            custom = {
+                cancelled = {
+                    raw = '[-]',
+                    rendered = '',
+                    highlight = 'RenderMarkdownCancelled',
+                    scope_highlight = 'RenderMarkdownCancelledScope',
+                },
+            },
+        },
     },
 }
