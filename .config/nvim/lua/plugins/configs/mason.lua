fd0f4e5a9e9b47de351e876e64ee477769f875c3
henryppercy <henryppercy@gmail.com>
2026-03-29 11:59:41 +0100

    style: fmt

--- .config/nvim/lua/plugins/configs/mason.lua
+++ .config/nvim/lua/plugins/configs/mason.lua
@@ -1,17 +1,17 @@
 local M = {
-	"williamboman/mason.nvim",
+    "williamboman/mason.nvim",
 }
 
 function M.init()
-	require("mappings").register({ "<leader>is", "<cmd>Mason<cr>", desc = "Servers panel" })
+    require("mappings").register({ "<leader>Is", "<cmd>Mason<cr>", desc = "Servers panel" })
 end
 
 function M.config()
-	require("mason").setup({
-		ui = {
-			border = "none", -- the border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-		},
-	})
+    require("mason").setup({
+        ui = {
+            border = "none", -- the border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+        },
+    })
 end
 
 return M
